d4b4310a63c6756c732101376a263831ef376b3e
huseyinekh <huseyinekh@code.edu.az>
2022-12-13 12:18:17 -0800

    add project

---
+++ OOP.Task 2/Models/Employe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Task_2.Models
+{
+    public class Employe
+    {
+
+        public string Username { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+
+        public string setUser(string name, string surname)
+        {
+            Username = $"{name} {surname}";
+            Email = $"{name}{surname}@code.edu.az";
+            Console.WriteLine($"{this.GetType().GetProperty("Email").Name}:{Email}");
+            return Email;
+        }
+    }
+}
---
+++ OOP.Task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using OOP.Task_2.Models;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main()
+    {
+        Employe t = new Employe();
+        //typeof(Employe).GetMethod("Hello").Invoke(t, new[] { "WOLLA" });
+
+        // alternative if you don't know the type of the object:
+        t.GetType().GetMethod("setUser").Invoke(t, new[] { "Huseyn","Khidirov" });
+
+    }
+}
+
+
+
